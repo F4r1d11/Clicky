29cc1421e2bf7c1e48f9a849d502a589225ed982
Farid <farid.delapena@gmail.com>
2019-08-05 18:46:50 -0500

    last

---
+++ build/precache-manifest.7b04fe9ffbe852177ee8e1cc6385242e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "631ac6cdeba310273fa6",
+    "url": "/Clicky/static/css/main.d7ee6e9e.chunk.css"
+  },
+  {
+    "revision": "631ac6cdeba310273fa6",
+    "url": "/Clicky/static/js/main.631ac6cd.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/Clicky/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "11525fd074178a5559c0",
+    "url": "/Clicky/static/js/runtime~main.11525fd0.js"
+  },
+  {
+    "revision": "427bddd2c0e27ce9141e8d11ce50788e",
+    "url": "/Clicky/index.html"
+  }
+];
